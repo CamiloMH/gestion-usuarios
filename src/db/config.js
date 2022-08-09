1eaee8da56f73378f31dc99718becfe92546b8c4
Camilo <camilo.mh2014@gmail.com>
2022-08-09 00:05:41 -0400

    🔧 add sequelize-cli config

---
+++ src/db/config.js
@@ -0,0 +1,52 @@
+import '../config/env.js'
+const {
+	DB_HOST,
+	DB_PORT,
+	DB_USER,
+	DB_PASS,
+	DB_NAME,
+	DB_DIALECT,
+	DB_NAME_TEST,
+} = process.env
+export default {
+	development: {
+		username: DB_USER,
+		password: DB_PASS,
+		database: DB_NAME,
+		host: DB_HOST,
+		port: DB_PORT,
+		dialect: DB_DIALECT,
+		logging: false,
+		define: {
+			timestamps: false,
+		},
+		dialectOptions: {
+			bigNumberStrings: true,
+		},
+	},
+	test: {
+		username: DB_USER,
+		password: DB_PASS,
+		database: DB_NAME_TEST,
+		host: DB_HOST,
+		port: DB_PORT,
+		dialect: DB_DIALECT,
+		define: {
+			timestamps: false,
+		},
+		dialectOptions: {
+			bigNumberStrings: true,
+		},
+	},
+	production: {
+		username: DB_USER,
+		password: DB_PASS,
+		database: DB_NAME,
+		host: DB_HOST,
+		port: DB_PORT,
+		dialect: DB_DIALECT,
+		dialectOptions: {
+			bigNumberStrings: true,
+		},
+	},
+}
