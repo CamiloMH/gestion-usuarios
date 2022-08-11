b75d83d77c1e49e2344892c843878facaeb21614
Camilo <camilo.mh2014@gmail.com>
2022-08-11 19:51:14 -0400

    ✨ add dto-types

---
+++ src/utils/dto-types.js
@@ -0,0 +1,34 @@
+import { Type } from '@sinclair/typebox'
+
+export const usernameDTO = Type.String({
+	minLength: 3,
+	maxLength: 40,
+	errorMessage: {
+		type: 'Username debe ser un string',
+		minLength: 'Username debe tener al menos 3 caracteres',
+		maxLength: 'Username no puede tener más de 40 caracteres',
+		required: 'Username es requerido',
+	},
+})
+
+export const passwordDTO = Type.String({
+	// password minimo 10 caracteres, maximo 20 y al menos una letra mayuscula, una minuscula y un número
+	format: 'password',
+	minLength: 10,
+	maxLength: 20,
+	errorMessage: {
+		format:
+			'Password debe tener al menos una letra mayuscula, una minuscula y un número',
+		type: 'Password debe ser un string',
+		minLength: 'Password debe tener al menos 10 caracteres',
+		maxLength: 'Password no puede tener más de 20 caracteres',
+	},
+})
+
+export const emailDTO = Type.String({
+	format: 'email',
+	errorMessage: {
+		type: 'Email debe ser un string',
+		format: 'Email no es válido',
+	},
+})
