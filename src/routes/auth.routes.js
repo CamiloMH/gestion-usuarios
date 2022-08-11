07d6f676706117e03fdae8814a163ff97cbbdf02
Camilo <camilo.mh2014@gmail.com>
2022-08-11 19:52:05 -0400

    ✨ feat: authentication routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,26 @@
 import { Router } from 'express'
-import { register } from '../controllers/auth.js'
+import { login, recoveryPassword, register } from '../controllers/auth.js'
+import {
+	loginDTO,
+	recoveryPasswordDTO,
+	registerDTO,
+} from '../middlewares/user-validate.js'
 
 const authRouter = Router()
 
-authRouter.post('/', register)
+/**
+ * @api {post} /auth/register Registrar un usuario
+ */
+authRouter.post('/register', registerDTO, register)
+
+/**
+ * @api {post} /auth/login Login de un usuario
+ */
+authRouter.post('/login', loginDTO, login)
+
+/**
+ * @api {post} /auth/recovery-password Recuperar contraseña
+ */
+authRouter.post('/recovery-password', recoveryPasswordDTO, recoveryPassword)
 
 export default authRouter
