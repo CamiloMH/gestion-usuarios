912135e40944bbd3affee3e5163bfc32006bf9bb
Camilo <camilo.mh2014@gmail.com>
2022-08-11 19:46:59 -0400

    🚧 WIP auth controllers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,11 @@
-
 export const register = (req, res) => {
-    res.send('Register')
+	res.send('Register')
+}
+
+export const login = (req, res) => {
+	res.send('Login')
+}
+
+export const recoveryPassword = (req, res) => {
+	res.send('RecoveryPassword')
 }
